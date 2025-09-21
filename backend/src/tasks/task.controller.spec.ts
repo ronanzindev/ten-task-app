3bda316f312d1a3dd76159b065c4e7eb47a7424c
ronanzindev <morellianogm@gmail.com>
2025-09-21 16:42:19 -0300

    test: find all test implementation

--- backend/src/tasks/task.controller.spec.ts
+++ backend/src/tasks/task.controller.spec.ts
@@ -14,11 +14,38 @@ describe("TaskController", () => {
 
     describe("create task", () => {
         it("should create and return a task", () => {
-            const dto = {title: "New 2025 Task", description: "2025 task goals", dueDate: new Date()}
+            const dto = { title: "New 2025 Task", description: "2025 task goals", dueDate: new Date() }
             const createTask = new Task(dto.title, dto.description, dto.dueDate)
             jest.spyOn(taskService, 'create').mockReturnValue(createTask)
             expect(taskController.create(dto)).toBe(createTask)
             expect(taskService.create).toHaveBeenCalledWith(dto)
         })
+    })
+    describe("findAll", () => {
+        it('should return an array of tasks', () => {
+            const result: Task[] = [
+                new Task("Task Title", "Task Description")
+            ]
+            jest.spyOn(taskService, 'findAll').mockImplementation(() => result)
+            expect(taskController.findAll()).toBe(result)
+        })
+        it('should return filtered tasks by title', () => {
+            const tasks: Task[] = [
+                new Task('Buy milk'),
+                new Task('Clean room'),
+                new Task('Buy chocolate'),
+            ];
+
+            jest.spyOn(taskService, 'findAllByTitle').mockImplementation((title: string) =>
+                tasks.filter((task) =>
+                    task.title.toLowerCase().includes(title.toLowerCase()),
+                ),
+            );
+
+            const result = taskController.findAll('buy');
+            expect(result).toHaveLength(2);
+            expect(result[0].title).toContain('Buy');
+            expect(result[1].title).toContain('Buy');
+        });
     })
 })
